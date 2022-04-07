60dd934de4e41ac752a27fced1b142e6c335c937
Brent <Brent.Flores@hotmail.com>
2022-04-07 07:25:11 -0400

    removed test script

--- createObjects.py
+++
@@ -1,9 +0,0 @@
-from polls.models import Question, Choice
-
-from django.utils import timezone
-
-Question.objects.all()
-
-q = Question(question_text="What's new?", pub_date=timezone.now())
-
-q.save()
